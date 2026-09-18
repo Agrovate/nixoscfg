7934f037193b19509da19e81cfc921b338f0a9ee
agrovate <nishantullal@icloud.com>
2026-09-18 15:04:02 +0530

    removal of btrfs and preservation

--- modules/hosts/BACKPAIN/disko.nix
+++ modules/hosts/BACKPAIN/disko.nix
@@ -1,56 +1,49 @@
-{...}: {
-  flake.diskoConfigurations.backpainFileSystem = {...}: {
-    fileSystems."/nix".neededForBoot = true;
-
+{
+  flake.diskoConfigurations.backpainFileSystem = {
     disko.devices = {
       disk.main = {
         device = "/dev/disk/by-id/nvme-PC_SN530_NVMe_WDC_256GB_22074K801654";
         type = "disk";
+
         content = {
           type = "gpt";
+
           partitions = {
             ESP = {
               name = "ESP";
-              size = "512M";
+              size = "1G";
               type = "EF00";
+
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = ["fmask=0022" "dmask=0022"];
-              };
-            };
-            swap = {
-              size = "8G";
-              content = {
-                type = "swap";
-                resumeDevice = true;
+                mountOptions = [
+                  "fmask=0022"
+                  "dmask=0022"
+                ];
               };
             };
             root = {
               name = "root";
               size = "100%";
+
               content = {
-                type = "btrfs";
-                extraArgs = ["-f"];
-                subvolumes = {
-                  "/persist" = {
-                    mountpoint = "/persist";
-                    mountOptions = ["compress=zstd" "noatime"];
-                  };
-                  "/nix" = {
-                    mountpoint = "/nix";
-                    mountOptions = ["compress=zstd" "noatime"];
-                  };
-                };
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+            swap = {
+              size = "8G";
+
+              content = {
+                type = "swap";
+                resumeDevice = true;
               };
             };
           };
         };
-      };
-      nodev."/" = {
-        fsType = "tmpfs";
-        mountOptions = ["size=25%" "mode=755"];
       };
     };
   };
